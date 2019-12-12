9c80ef6326e08eceb13fbc0efe01c549b7809db2
elidje aka emmanuel <akaelidje@consultech.ci>
2019-12-12 09:24:10 +0000

    should round values

--- test/percentageSpec.js
+++ test/percentageSpec.js
@@ -7,5 +7,13 @@ describe("percentage",()=>{
             assert.equal(Percentage.evolution(100,150),50)
             assert.equal(Percentage.evolution(100,50),-50)
         })
+
+        it('should handle 0 evolution',()=>{
+            assert.equal(Percentage.evolution(0,100),Infinity)
+        })
+
+        it('should round values',()=>{
+            assert.equal(Percentage.evolution(30,100),233.33)
+        })
     })
 })
