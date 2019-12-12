be275988ecafb21d14071f255b26f27ec9d8f795
elidje aka emmanuel <akaelidje@consultech.ci>
2019-12-12 08:30:58 +0000

    Test de text

---
+++ test/demoSpec.js
@@ -0,0 +1,7 @@
+var assert = require('assert')
+describe('Test de text',()=>{
+    it('should do something',()=>{
+        a=3
+        assert.equal(a*2,6,'La multiplication n\'a pas fonctionné')
+    })
+})
