818423f277492678d7eb6c0937dffd3faf938b61
elidje aka emmanuel <akaelidje@consultech.ci>
2019-12-12 08:56:27 +0000

    create lib for percentage evolution

--- test/percentageSpec.js
+++ test/percentageSpec.js
@@ -1,5 +1,5 @@
 var assert = require('assert');
-var Percentage = require('../../lib/Percentage');
+var Percentage = require('../lib/Percentage');
 describe("percentage",()=>{
     describe("#evolution",()=>{
         it('should give an evolution',()=>{
