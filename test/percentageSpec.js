fb956c76f2abb8c7fb71301f2724cabecc185402
elidje aka emmanuel <akaelidje@consultech.ci>
2019-12-12 08:48:32 +0000

    add a test for percentage

---
+++ test/percentageSpec.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+var Percentage = require('../../lib/Percentage');
+describe("percentage",()=>{
+    describe("#evolution",()=>{
+        it('should give an evolution',()=>{
+            assert.equal(Percentage.evolution(100,200),100)
+            assert.equal(Percentage.evolution(100,150),50)
+            assert.equal(Percentage.evolution(100,50),-50)
+        })
+    })
+})
